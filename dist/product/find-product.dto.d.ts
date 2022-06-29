868c30038cb7f1a63645ae0104d913e40791fb9a
digDev <minyaevmail@gmail.com>
2022-06-29 14:23:08 +0300

    first_settings_add_modules_models_controllers_dto

---
+++ dist/product/find-product.dto.d.ts
@@ -0,0 +1,4 @@
+export declare class findProductDto {
+    category: string;
+    limit: number;
+}
---
+++ dist/product/find-product.dto.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.findProductDto = void 0;
+class findProductDto {
+}
+exports.findProductDto = findProductDto;
+//# sourceMappingURL=find-product.dto.js.map
